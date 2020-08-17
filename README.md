49d4ca990f27e8f664d9993aed7ae919e8ed7dc0
Murtuza Kainan <murtuzakainan@gmail.com>
2020-08-17 02:52:40 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# TEAL: TE(xt) A(dventure) L(oader)
+
+A simple Rust program that allows one to play text adventures.
+
+## TEAL Schemas
+
+TEAL uses the following JSON schema to load text adventures:
+
+```
+{
+    name: String,
+    start_node: String,
+    nodes: [
+        {
+            id: String,
+            prompt: String,
+            options: [
+                { option: String, node_id: String }, ...
+            ]
+        },
+        ...
+    ]
+}
+```
+
+Each adventure is graph of connecting nodes that are each identified by a `node_id`. 
---
+++ src/main.rs
@@ -0,0 +1,138 @@
+use std::env;
+use std::fs;
+use std::io::{self, Write};
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+use serde_json::Result;
+
+// TEAL Schema.
+#[derive(Serialize, Deserialize)]
+struct TealOption {
+    option  : String,
+    node_id : String
+}
+
+#[derive(Serialize, Deserialize)]
+struct TealNode {
+    id      : String, 
+    prompt  : String,
+    options : Vec<TealOption>
+}
+
+#[derive(Serialize, Deserialize)]
+struct TealSchema {
+    name       : String,
+    start_node : String,
+    nodes      : Vec<TealNode>
+}
+
+// We define a custom result type so that we can deal with errors.
+type TealResult<T> = std::result::Result<T, &'static str>;
+
+fn find_teal_node<'a>(nodes: &'a Vec<TealNode>, id: &'a String) -> TealResult<&'a TealNode> {
+    for node in nodes.iter() {
+        if node.id.eq(id) {
+            return Ok(node);
+        }
+    }
+    return Err("Could not find TEAL node.");
+}
+
+fn teal_prompt<'a>() -> TealResult<u32> {
+    let mut selection = String::new();
+    io::stdin()
+        .read_line(&mut selection)
+        .expect("Failed to read line.");
+
+    match selection.trim().parse::<u32>() {
+        Ok(number) => return Ok(number),
+        Err(_e)     => {
+            return Err("Enter a valid option number!")
+        }
+    }
+}
+
+fn flush_output() {
+    match io::stdout().flush() {
+        Ok(_r)  => return,
+        Err(_e) => panic!("Could not flush to stdout!") 
+    }
+}
+
+fn process_node<'a>(nodes: &'a Vec<TealNode>, node: &'a TealNode) {
+    println!("\n{}", &node.prompt);
+
+    let mut option_counter = 1;
+    let mut option_map     = HashMap::new();
+    for option in node.options.iter() {
+        println!("{}: {}", option_counter, &option.option);
+        option_map.insert(option_counter, &option.node_id);
+        option_counter += 1;
+    }
+
+    if option_map.is_empty() {
+        println!("And thus ends this adventure!");
+        return;
+    }
+
+    println!("\nHow do you want to proceed? (Pick a number)");
+    print!("Option>");
+    flush_output();
+
+    let mut selected        = false;
+    let mut selected_option = 0;
+    while !selected {
+        let selection = teal_prompt();
+        match selection {
+            Ok(number) => {
+                if option_map.contains_key(&number) {
+                    selected        = true;
+                    selected_option = number;
+                }
+                else {
+                    println!("{} is not in the list of options!", number);
+                    print!("Option>");
+                    flush_output();
+                }
+                continue;
+            },
+            Err(e) => {
+                println!("{}", e);
+                print!("Option>");
+                flush_output();
+            }
+        }
+    }
+
+    match option_map.get(&selected_option) {
+        Some(node_id) => {
+            let next_node = find_teal_node(nodes, node_id);
+            match next_node {
+                Ok(new_node) => process_node(nodes, new_node),
+                Err(e)       => panic!(e)
+            }
+        },
+        None => panic!("You selected an invalid option!")
+    }
+}
+
+fn main() -> Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let file_name         = &args[1];
+    let contents          = fs::read_to_string(file_name)
+                                .expect("Something went wrong reading the file");
+
+    let teal: TealSchema = serde_json::from_str(&contents)?;
+    println!("Text Adventure Loaded: {}\n.\n.\n.\n", teal.name);
+
+    let story_nodes = teal.nodes;
+    let start_node  = find_teal_node(&story_nodes, &teal.start_node);
+    let result      = match start_node {
+        Ok(node) => node,
+        Err(e)   => panic!(e)
+    };
+
+    process_node(&story_nodes, result);
+    Ok(())
+}
